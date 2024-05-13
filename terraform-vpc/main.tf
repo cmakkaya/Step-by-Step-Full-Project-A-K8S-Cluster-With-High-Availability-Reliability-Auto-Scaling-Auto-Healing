041addfdac907683c6a0ec7ae879e10fec07fab1
cumhur <cmakkaya.aws@gmail.com>
2024-05-13 21:05:28 +0300

    changed vpc fo eks files

--- terraform-vpc/main.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "name" {}
-
-module "vpc" {
-  source = "./vpc"
-  name   = var.name
-  region = var.region
-}
--- terraform-vpc/vpc/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  eks_cluster_name = var.eks_cluster_name != "" ? var.eks_cluster_name : var.name
-}
